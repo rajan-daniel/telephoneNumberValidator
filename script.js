2b44a89c912ed57d34b97b2ac4557a4bdcc3ae74
Rajan Daniel <rajandaniel118@gmail.com>
2026-03-03 23:00:27 -0600

    work logic

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ const clearBtn = document.getElementById("clear-button");
 
 // functions
 //add <li> to history UL>
+const addResult = (input) => {
+    const li = document.createElement("li");
+    li.textContent = input;
+    results.appendChild(li);
+};
 
 //#isValid checker
-
+const isValid = (input) => {
+    //check if letters or anything not # insta return false
+    const notNumbers = new RegExp("[^0-9]");
+    return !notNumbers.test(input);
+}
+console.log(isValid(""));
 //update page when BTN clicked clear or other
 
 
