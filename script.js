89d383d7d74c6f4ff2092d20e52a72a49e44a4a9
Rajan Daniel <rajandaniel118@gmail.com>
2026-03-03 22:39:44 -0600

    blueprint

---
+++ script.js
@@ -0,0 +1,22 @@
+// get elements
+const input = document.getElementById("textInput");
+const results = document.getElementById("history");
+const checkBtn = document.getElementById("check-button");
+const clearBtn = document.getElementById("clear-button");
+
+// functions
+//add <li> to history UL>
+
+//#isValid checker
+
+//update page when BTN clicked clear or other
+
+
+
+
+
+
+
+// resultants
+
+// extras
